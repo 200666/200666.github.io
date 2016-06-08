cf4f1c38df707f489d82d56330751c29ac732878
200666 <200666@hkis.edu.hk>
2016-06-08 08:31:19 +0800

    added game

---
+++ game/gamedev.js
@@ -0,0 +1,179 @@
+$( document ).ready(function() {
+$(".question").hide();
+$(".choice").hide();
+$(".gameover").hide();
+$("#q3").hide();
+$("#over2").hide();
+
+$("#ready").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://images.huffingtonpost.com/2016-01-05-1452004586-8964259-12036769_1223251501035058_7389760182587362890_n.jpg)");
+$("#ready").hide();
+$("#q1").show();
+$("#c1").show();
+$("#c2").show();
+$("#over1").hide();
+$("#q3").hide();
+$("#q2").hide();
+ });
+$("#c1").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://orig15.deviantart.net/7381/f/2009/144/f/b/game_over_screen_wallpaper_by_patryk_ludamage.jpg)");
+$(".question").hide();
+$("#over1").show();
+$("#over2, #over3").hide();
+ });
+$("#c3").click(function(){
+$("#realdealBody").css("backgroundImage","url(https://i.vimeocdn.com/video/507439574_1280x720.jpg)");
+$(".gameover").hide();
+$("#q1").hide();
+$("#q3").show();
+$("#q2").hide();
+
+ });
+
+
+$("#c2").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://65.media.tumblr.com/a31344aad5a3584349720308a0182f1d/tumblr_inline_o7eeb8cDjh1r8mwrd_500.jpg)");
+$(".gameover").hide();
+$("#q1").hide();
+$("#q2").show();
+$("#q3").hide();
+
+ });
+$("#c4 , #c5").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://cdn-gd.playbill.com/images/cache/remote/http_cdn-images.playbill.com/ee_assets/cam/abbigail/month/prodhamilton0040rr.jpg)");
+$(".gameover").hide();
+$("#q1").hide();
+$("#q2").hide();
+$("#q3").hide();
+$("#q4").show();
+
+ });
+
+$("#c6").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://3a376o1lveli4brgjcn2y118.wpengine.netdna-cdn.com/wp-content/uploads/2015/08/couple.jpg)");
+$(".gameover").hide();
+$("#q1").hide();
+$("#q2").hide();
+$("#q3").hide();
+$("#q4").hide();
+$("#q5").show();
+
+ });
+
+$("#c7").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://www.trbimg.com/img-55cb8fcc/turbine/la-et-cm-hamilton-broadway-box-office-20150812)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6 ").hide();
+$(".gameover").hide();
+$("#q7").show();
+ });
+
+$("#c8").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://orig15.deviantart.net/7381/f/2009/144/f/b/game_over_screen_wallpaper_by_patryk_ludamage.jpg)");
+$(".question").hide();
+$("#over2").show();
+$("#over1").hide();
+ });
+
+
+$("#c9").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://orig15.deviantart.net/7381/f/2009/144/f/b/game_over_screen_wallpaper_by_patryk_ludamage.jpg)");
+$(".question").hide();
+$("#over3").show();
+$("#over1").hide();
+$("#over2").hide();
+ });
+$("#c10").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://graphics8.nytimes.com/images/2015/03/19/theater/19hamilton/19hamilton-facebookJumbo.jpg)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6, #q7 ").hide();
+$(".gameover").hide();
+$("#q8").show();
+ });
+$("#c11").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://www.bravotv.com/sites/nbcubravotv/files/styles/blog-post--computer/public/dish-hamilton-02.jpg?itok=TYA0ghJO)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6, #q7, #q8 ").hide();
+$(".gameover").hide();
+$("#q9").show();
+ });
+$("#c12").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://graphics8.nytimes.com/images/2015/11/13/theater/13artsbeat-hamilton/13artsbeat-hamilton-facebookJumbo.jpg)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6, #q7, #q8 , #q9").hide();
+$(".gameover").hide();
+$("#q10").show();
+ });
+$("#c13").click(function(){
+$("#realdealBody").css("backgroundImage","url(https://pbs.twimg.com/media/CVJirc6UwAA-flK.jpg)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6, #q7, #q8 , #q9 , #q10 ").hide();
+$(".gameover").hide();
+$("#q11").show();
+ });
+$("#c14").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://orig15.deviantart.net/7381/f/2009/144/f/b/game_over_screen_wallpaper_by_patryk_ludamage.jpg)");
+$(".question").hide();
+$("#over4").show();
+$("#over2, #over3 , #over1").hide();
+ });
+$("#c15").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://assets.rollingstone.com/assets/2016/article/hamilton-cast-to-perform-on-grammys-via-satellite-20160203/226661/medium_rect/1454511931/720x405-Hamilton0044r.jpg)");
+$(".question").hide();
+$("#over5").show();
+$("#over2, #over3 , #over1 , #over4").hide();
+ });
+$("#c16").click(function(){
+$("#realdealBody").css("backgroundImage","url(https://pbs.twimg.com/media/CUcA8-lWsAQABDo.jpg:large)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6, #q7, #q8 , #q9 , #q10, #q11 ").hide();
+$(".gameover").hide();
+$("#q12").show();
+ });
+$("#c15").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://orig15.deviantart.net/7381/f/2009/144/f/b/game_over_screen_wallpaper_by_patryk_ludamage.jpg)");
+$(".question").hide();
+$("#over6").show();
+$("#over2, #over3 , #over1 , #over4 , #over5").hide();
+ });
+$("#c17").click(function(){
+$("#realdealBody").css("backgroundImage","url(https://images.rapgenius.com/7d000ab35d6043376a6c5fc95933d028.997x732x1.png)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6, #q7, #q8 , #q9 , #q10, #q11 , #q12 ").hide();
+$(".gameover").hide();
+$("#q13").show();
+ });
+$("#c18").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://orig15.deviantart.net/7381/f/2009/144/f/b/game_over_screen_wallpaper_by_patryk_ludamage.jpg)");
+$(".question").hide();
+$("#over6").show();
+$("#over2, #over3 , #over1 , #over4 , #over5").hide();
+ });
+$("#c21").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://orig15.deviantart.net/7381/f/2009/144/f/b/game_over_screen_wallpaper_by_patryk_ludamage.jpg)");
+$(".question").hide();
+$("#over7").show();
+$("#over2, #over3 , #over1 , #over4 , #over5 , #over6 , ").hide();
+ });
+$("#c19").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://guidelive.imgix.net/1453413131-THEATER-BRANTLEY-ADV13-2.jpg)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6, #q7, #q8 , #q9 , #q10, #q11 , #q12, #q13 ").hide();
+$(".gameover").hide();
+$("#q14").show();
+ });
+$("#c20").click(function(){
+$("#realdealBody").css("backgroundImage","url(https://cdn.theatlantic.com/assets/media/img/mt/2015/08/HamiltonBway0341r_Daveed_Diggs_as_Thomas_Jefferson_and_the_ensemble_of_HAMILTON/facebook.jpg?1439921221)");
+$("#q1 , #q2 , #q3 , #q4 , #q5 , #q6, #q7, #q8 , #q9 , #q10, #q11 , #q12, #q13 , #q14 ").hide();
+$(".gameover").hide();
+$("#q15").show();
+ });
+$("#c23").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://assets.rollingstone.com/assets/2016/article/hamilton-cast-to-perform-on-grammys-via-satellite-20160203/226661/medium_rect/1454511931/720x405-Hamilton0044r.jpg)");
+$(".question").hide();
+$("#over8").show();
+$("#over2, #over3 , #over1 , #over4 , #over5 , #over6 , #over7").hide();
+ });
+$("#c22").click(function(){
+$("#realdealBody").css("backgroundImage","url(http://orig15.deviantart.net/7381/f/2009/144/f/b/game_over_screen_wallpaper_by_patryk_ludamage.jpg)");
+$(".question").hide();
+$("#over9").show();
+$("#over2, #over3 , #over1 , #over4 , #over5 , #over6 , #over7 , #over8").hide();
+ });
+//Your jquery goes here
+
+
+});
+
